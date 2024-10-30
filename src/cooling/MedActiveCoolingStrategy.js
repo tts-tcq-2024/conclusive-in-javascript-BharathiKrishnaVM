e3c447eed26da3eaf5660eeee707adfbefd9ab84
Bharathi Krishna V M <49585360+BharathiKrishnaVM@users.noreply.github.com>
2024-10-30 13:16:37 +0530

    update

--- src/cooling/MedActiveCoolingStrategy.js
+++
@@ -1,9 +0,0 @@
-const CoolingTypeStrategy = require('./CoolingTypeStrategy');
-
-class MedActiveCoolingStrategy extends CoolingTypeStrategy {
-  constructor() {
-    super(0, 40);  // Passing the specific limits to the parent class
-  }
-}
-
-module.exports = MedActiveCoolingStrategy;
